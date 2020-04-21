9307826a66c77a6d73fb29f58370cc3651b77c6a
kazuhitonakayama <firstjin19990101@gmail.com>
2020-04-21 15:11:04 +0900

    Finished Udemy _29

--- src/jp/learningdesign/javastudy/StaffInfo.java
+++ src/jp/learningdesign/javastudy/StaffInfo.java
@@ -1,10 +1,18 @@
 package jp.learningdesign.javastudy;
 
+class Staff {
+	String name;
+	int staffid;
+	String email;
+}
 public class StaffInfo {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		Staff yamada = new Staff();
+		yamada.name = "Kazuhito Nakayama";
+		
+		System.out.println(yamada.name);
 	}
 
 }
