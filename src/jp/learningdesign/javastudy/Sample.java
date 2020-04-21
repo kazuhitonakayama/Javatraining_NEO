b4830e963851f67f14caa0bbf70cc9a442e169b8
kazuhitonakayama <firstjin19990101@gmail.com>
2020-04-21 15:03:13 +0900

    Creating StaffInfo

---
+++ src/jp/learningdesign/javastudy/Sample.java
@@ -0,0 +1,9 @@
+package jp.learningdesign.javastudy;
+
+public class Sample {
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello World!!");
+		System.out.print("あああああ");
+	}
+}
