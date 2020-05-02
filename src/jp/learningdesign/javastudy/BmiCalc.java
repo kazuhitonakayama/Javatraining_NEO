068478bc22b871eae7da73251a61cbf798e32ecc
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-02 23:11:20 +0900

    _48_finished_and_learn_that_BMI_App

--- src/jp/learningdesign/javastudy/BmiCalc.java
+++ src/jp/learningdesign/javastudy/BmiCalc.java
@@ -1,5 +1,5 @@
 package jp.learningdesign.javastudy;
-
+import java.util.Scanner;
 public class BmiCalc {
 
 	public static void main(String[] args) {
@@ -7,31 +7,36 @@ public class BmiCalc {
 		/*
 		 * BMIを計算するアプリ
 		 * BMI = 体重/(身長*身長)
-		 * 
+		 *
 		 */
 		double weight,height,bmi;
-		
-		weight = 85;
-		height = 180;
-		
+
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("体重は? (kg)");
+		weight = stdIn.nextDouble();
+		System.out.println("身長は? (cm)");
+		height = stdIn.nextDouble();
+		// weight = 85;
+		// height = 180;
+
 		height /= 100;
-		
+
 		bmi = weight/(height*height);
-		
-		System.out.println(bmi);
-		
+
+		System.out.println("あなたのBMIは" + bmi + "です");
+
 		if (bmi < 18) {
-			System.out.println("痩せ型");
+			System.out.println("そして、痩せ型です");
 		} else if (bmi < 24) {
-			System.out.println("普通");
+			System.out.println("そして、普通です");
 		} else {
-			System.out.println("肥満");
+			System.out.println("そして、肥満です");
 		}
 		/*
 		 * 代入演算子
 		 * 左右は同じ結果
 		 * x += Y, = , x = x + y
-		 * 
+		 *
 		 */
 	}
 
