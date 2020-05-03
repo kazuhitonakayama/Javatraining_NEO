a33a34056764361af98e40b8af8cc5304ae99123
kazuhitonakayama <firstjin19990101@gmail.com>
2020-05-03 16:19:36 +0900

    _52_finished_NOT演算子

--- src/jp/learningdesign/javastudy/CommonMultiple3.java
+++ src/jp/learningdesign/javastudy/CommonMultiple3.java
@@ -1,13 +1,13 @@
 package jp.learningdesign.javastudy;
 
-public class CommonMultiple2 {
+public class CommonMultiple3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		// 2と3の公倍数以外の数を表示する
+		// 2または3の倍数を1から50未満の間で表示する
 		int i = 1;
-		while (i < 20) {
-			if (!((i % 2 == 0) && (i % 3 == 0))) {
+		while (i < 50) {
+			if ((i % 2 == 0) || (i % 3 == 0)) {
 				System.out.println(i);
 			}
 			i++; // インクリメント演算子
